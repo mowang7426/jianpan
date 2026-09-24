045fe569481e67ae69e579a6159b0410f1fdbb29
mowang7426 <972565726@qq.com>
2026-09-25 00:49:05 +0800

    Add files via upload

--- RKAdaptivePerformance.h
+++ RKAdaptivePerformance.h
@@ -1,5 +1,16 @@
+#pragma once
 #import <Foundation/Foundation.h>
+
+// Shared by Objective-C (.m) and Objective-C++ (Logos .xm).
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 // Main-thread only. Session-local state, no text capture or disk writes.
 void RKAdaptiveSetEnabled(BOOL enabled);
 void RKAdaptiveNoteInput(void);
 NSInteger RKAdaptiveLevel(void);
+
+#ifdef __cplusplus
+}
+#endif
