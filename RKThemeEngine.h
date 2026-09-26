e69d21434f6379a3705a1bcf33bdaa9b495223f6
mowang7426 <monkeycode-ai@chaitin.com>
2026-09-26 16:08:55 +0000

    fix: link theme engine from Objective-C++ Co-authored-by: monkeycode-ai <monkeycode-ai@chaitin.com>

--- RKThemeEngine.h
+++ RKThemeEngine.h
@@ -1,4 +1,13 @@
 #import <Foundation/Foundation.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 NSDictionary *RKThemeDefinition(NSInteger theme);
 NSString *RKThemeDisplayName(NSInteger theme);
 NSDictionary *RKThemeMergedPreferences(NSDictionary *preferences);
+
+#ifdef __cplusplus
+}
+#endif
